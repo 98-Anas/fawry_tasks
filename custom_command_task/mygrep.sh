91de73bc07547c88a16d3b598a543d1bb6c93a45
98-Anas <algalad.anas@gmail.com>
2025-04-28 15:19:11 +0300

    Added core processing functions

--- custom_command_task/mygrep.sh
+++ custom_command_task/mygrep.sh
@@ -17,3 +17,40 @@ function handle_error() {
     echo "Usage: $0 [OPTIONS] PATTERN [FILE]" >&2
     exit 1
 }
+
+# Function to process the input file/stream
+function process_input() {
+    local line_num=0
+    
+    while IFS= read -r line; do
+        ((line_num++))
+        
+        if [[ "${line,,}" == *"${pattern,,}"* ]]; then
+            match_found=1
+        else
+            match_found=0
+        fi
+        
+        if ((invert_match)); then
+            if ((!match_found)); then
+                print_line "$line_num" "$line"
+            fi
+        else
+            if ((match_found)); then
+                print_line "$line_num" "$line"
+            fi
+        fi
+    done < "$input_source"
+}
+
+# Function to print lines with optional line numbers
+function print_line() {
+    local num=$1
+    local line=$2
+    
+    if ((line_numbers)); then
+        echo "$num:$line"
+    else
+        echo "$line"
+    fi
+}
